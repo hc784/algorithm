f93a97e45599188dbf529ad05c79adc4416ee0c8
hc784 <151542190+hc784@users.noreply.github.com>
2024-09-05 18:27:07 +0900

    [Silver I] Title: 회의실 배정, Time: 32 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,20 +1,39 @@
-#include<bits/stdc++.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include <list>
 using namespace std;
-pair<int,int>arr[100001];
-int main(){
-    int n,a,b,cnt=1;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>b>>a;
-        arr[i]={a,b};
-    }
-    sort(arr,arr+n); 
-    int end=arr[0].first;
-    for(int i=1;i<n;i++){
-        if(end<=arr[i].second){ 
-            cnt++;
-            end=arr[i].first;
-        }
-    }
-    cout<<cnt;
+pair<int, int> map[100000];
+//vector<pair<int, int>> map;
+bool compare(pair<int, int> i, pair<int, int> j) { return i.second < j.second; }
+
+int main() { // 끝 또는 시작이 같을 경우 더 짧은 것만 남긴다.
+	int n;
+	cin >> n; 
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &map[i].second, &map[i].first);
+	}
+	sort(map,&map[n]);
+	
+	int endTime = map[0].first;;
+	int cnt = 1;
+	
+	for (int i = 1; i < n; i++) {
+		if (map[i].second >= endTime)
+		{
+			cnt++;
+			endTime = map[i].first;
+
+		}
+	}
+
+	cout << cnt;
+	//for (int i = 0; i < n; i++) {
+	//	cout << map[i].first << " , " << map[i].second<<endl;
+	//}
+
 }
