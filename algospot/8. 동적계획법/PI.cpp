eae53f5afbda59a9cad61c57022eeebe8e4f037c
hc784 <kkjj1211@naver.com>
2026-02-06 22:08:57 +0900

    주석 추가

--- algospot/8. 동적계획법/PI.cpp
+++ algospot/8. 동적계획법/PI.cpp
@@ -11,7 +11,7 @@ int cache[10004];
 
 int a(int begin, int num)
 {
-  
+    //cout<<"pi : "<<pi.substr(begin,num+1)<< "    cache[10] : "<< cache[10] <<"    cache[13] : " <<  cache[13] << endl;
     int& ret=cache[begin+num+1];
 
     if(pi.size()==begin)
@@ -32,7 +32,15 @@ int a(int begin, int num)
     string M = pi.substr(begin,num+1);
     
     if(M==string(M.size(),M[0])) return 1+ (ret = min(ret,min({a(begin+num+1,2),a(begin+num+1,3),a(begin+num+1,4)})));
-
+  /*
+  문제점 ret에 한턴 빠르게 들어가버림 
+  고치려면? 원래는 666 6666 66662 각 값을 비교하고 cache 3에 넣어야하는데 지금은 666에서 뒤에 
+  세개가 끝난 뒤에 바로 바로 666을 더하고 바로 ret에 들어감
+해결책 
+1. cache에 저장할 begin값을 뒤로 땡기고 ,,그리고 아 너 무 복 잡해 
+2. begin 값을 앞으로 땡긴다. 땡기려면 앞 재귀의 num값이 필요하다. 이때 num 값을 어떻게 구하냐? <- 폐기
+그냥 그자리에 캐쉬 값 저장하는게 
+  */
     // 단조증감
     bool progressive = true;
     for(int i=0;i<M.size()-1;i++)
@@ -85,6 +93,12 @@ int a(int begin, int num)
 두 개의 숫자가 번갈아 가며 출현할 때 (예: 323, 54545) 난이도: 4
 숫자가 등차 수열을 이룰 때 (예: 147, 8642) 난이도: 5
 그 외의 경우 난이도: 10
+
+12345666661234111123135 
+13
+
+1
+786432113265412345678
 */
 
 
@@ -105,3 +119,11 @@ int main()
 
     return 0;
 }
+/*
+5 
+12341234 
+11111222 
+12122222 
+22222222 
+12673939 
+*/
--- algospot/8. 동적계획법/WILDCARD.cpp
+++ algospot/8. 동적계획법/WILDCARD.cpp
@@ -13,11 +13,22 @@ bool wildcard(string wildcard_str, string FileName , int wildcard_index , int fi
     if(wildcard_str[wildcard_str.size()-1]!='*'&&wildcard_str[wildcard_str.size()-1]!='?'&&wildcard_str[wildcard_str.size()-1]!=FileName[FileName.size()-1])
     {
         return false;
-    } //  끝 글자가 문자일때 다르면 false 
+    } // 이거 만든 의미? 끝 글자가 문자일때 다르면 false 
     for(int i = wildcard_index ; i<wildcard_str.size(); i++)
     {
 
-           
+            /*
+            // 와일드 카드가 뒤에 남아있는데 파일 인덱스가 끝났다고 브레이크? 그럼 별 안만났을 경우에는 확정 true인데. false 나와야함
+            // 이게 true가 나오는 겨웅가있나? 근데 파일 인덱스가 끝나는 경우는 .. 물음표나 문자 만나서 파일인덱스++ 됏을 경우. 이경우는 true 나오는게 맞음
+            // 근데 별 만나서 끝까지 파일 인덱스 돌리고 끝나면 ret 을 반환하게 되네.
+            // 뒤에 와일드 카드가 남았는데 문자가 끝날 경우는? true가 나온다. 버그 발견
+            // 와일드 카드가 남았을 때 그 남은 글자가 별이면 상관없음. 오류가 너무 많은데 이걸 어케 다고치냐.
+            인덱스 검사는 필수로 있어야하는데 별일 경우에만 인덱스 검사 넣어두고 ret 반환하게 만들기, 문자가 남아있는데 인덱스가 끝났다? 그럼 false반환, 물음표인데 인덱스 끝났다? false 반환
+1
+ab*absd*kc*acas*g
+1
+ababsdwascabsdaskasdkcasdacasdkasdkg
+        */
         if(wildcard_str[i]=='*')  // 두번째 경우 파일명에 문자가 있는데 와일드 카드가 끝난경우
         {
             
@@ -43,7 +54,17 @@ bool wildcard(string wildcard_str, string FileName , int wildcard_index , int fi
         {
             if(FileName.size()-1<file_index)
                 return false; // 파일명에 남은 글자가 없는데 물음표를 만났을 경우
-            file_index++; 
+            file_index++; /* 지금은 파일명 끝에 파일 인덱스 가있으면 와일드 카드 ? 랑 파일 인덱스가 매치 되지 않아도 브레이크 
+            되면서 트루 반환함 oktrue 있으면 ret 반환하고. 
+            와일드 카드가 물음표인데 파일명에 글자가 없다? 그럼 펄스가 나와야함. 물음표일때 파일명의 마지막 글자라면, 마지막 글자가 있다면 true. 앞이 다맞앗을 경우에.
+            마지막 글자일대늦ㄴ 파일 인덱스 ++ 되몀ㄴ서 파일 네임 사이즈랑 같아지고, 브레이크 되면서 true 반환
+            
+            */
+             // 파일 인덱스가 파일명보다 길어졌지만 아직 남은 와일드 카드가 있으면 false 근데 그 남은 와일드 카드가 *이 아니여야함. 하나남았고 
+            //하나 남은게 별이라면 true가 나와야함
+            //근데 파일 인덱스도 끝났고, 와일드 카드도 같이 끝났을 경우가 true. 그때는 브레이크
+            //근데 와일드 카드에 남은게 있을 때 그게 별 하나가 아니면서(이건 별 안에 코딩 되어있음. 따로 코딩할 필요 x) 파일명에 남은게 없는경우는 제일 위에 코딩하는게 맞는듯.
+            //그럼 여기서 
              
         }
         
@@ -127,3 +148,41 @@ int main()
 
     return 0;
 }
+
+
+
+/*
+물음표 생략 가능한지 
+물음표 생략 안되게 변경?
+변경할거 
+1. ? 만났을 때 무조건 하나 뛰는걸로 돼있는데. 이걸 생략이 가능하게 바꿔볼까? 이건 아닌거 같고
+2. 생략 불가능하게 바꾸려면
+1. 
+6
+he?p
+3
+help
+heap
+helpp
+*p*
+3
+help
+papa
+hello
+*bb*
+1
+babbbc
+t*l?*o*r?ng*s
+3
+thelordoftherings
+tlorngs
+tllorrngs
+******a
+2
+aaaaaaaaaab
+abcdea
+a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a
+2
+ahfjsdhfjkdshfkjdshfkdsaojajfjaljaflkajflkdsaljflkaflsaffasfa54656454aafasfsdafsaaaaaaaaaaaaaaaaaaaa
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+*/
